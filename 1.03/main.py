c72de234a29e65cb55baf3cc598f6665e4633f98
malikhass <136368985+malikhass@users.noreply.github.com>
2023-09-18 18:33:08 -0500

    Create main.py

---
+++ 1.03/main.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import time
+
+
+def main():
+    os.system('clear')
+    os.system('cls')
+    print("About me!\n")
+    time.sleep(1)
+    print("My legal name is Malik Khalid Abdalla Hassan.")
+    time.sleep(1)
+    print("I am a Software Engineer that specializes in Python.")
+    time.sleep(1)
+    print("I was born May 13, 2009 in Washington D.C. but spent my life in Berkeley, California.")
+    time.sleep(1)
+    print("Me and my mother moved to Green Bay in late 2021.")
+    time.sleep(1)
+    print("I am exicted to advance my career in Software Engineering!")
+    time.sleep(10)
+    os.system('clear')
+    os.system('cls')
+    sys.exit()
+main()
+
+# Prints info about me!
+# Creative Commons Attribution-NonCommercial 4.0 International
+# Do not reupload without significant changes, do not use in ANY way for commercial ventures.
