8c4303dcb43534a5f14903c77bfb07d8053de823
malikhass <136368985+malikhass@users.noreply.github.com>
2023-09-18 19:16:04 -0500

    Create main.py

---
+++ 1.04/main.py
@@ -0,0 +1,28 @@
+import os
+import sys
+import time
+from googlesearch import search
+import requests
+
+def main():
+    os.system('clear')
+    os.system('cls')
+    print("1.04 Input Responder\n")
+    print("My name is Malik!")
+    time.sleep(1)
+    ni = input("What is your name? ")
+    time.sleep(1)
+    print("Nice to meet you " + ni + "!")
+    time.sleep(1)
+    ci = input("What is your favorite color? ")
+    time.sleep(1)
+    print("Wow you have amazing taste, " + ci + " is an amazing color!")
+    time.sleep(10)
+    os.system('clear')
+    os.system('cls')
+    sys.exit()
+main()
+
+# Has a conversation with you about your local attractions.
+# Creative Commons Attribution-NonCommercial 4.0 International
+# Do not reupload without significant changes, do not use in ANY way for commercial ventures.
