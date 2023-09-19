ee558f0fa860cca1eb6b3c4556c33a94f59a4767
malikhass <136368985+malikhass@users.noreply.github.com>
2023-09-19 14:00:07 -0500

    Update main.py

--- 2.03/main.py
+++ 2.03/main.py
@@ -10,12 +10,12 @@ os.system('clear')
 os.system('cls')
 
 print("Coded by Malik.")
-print("Todays date is: " + str(currentDate))
+print("Today's date is: " + str(currentDate))
 
 print("\nThis program calculates the volume and surface area of a cylinder.\n")
 
-radius = float(input("Enter the radius (r) of the cylinder: "))
-height = float(input("Enter the height (h) of the cylinder: "))
+radius = float(input("Enter the radius (in meters, for example) of the cylinder: "))
+height = float(input("Enter the height (in meters, for example) of the cylinder: "))
 
 # Calculate the surface area of the cylinder
 lateralSurfaceArea = 2 * math.pi * radius * height
@@ -24,13 +24,9 @@ totalSurfaceArea = lateralSurfaceArea + baseArea
 
 volume = math.pi * radius**2 * height
 
-print(f"The surface area of the cylinder is: {totalSurfaceArea:.2f}")
-print(f"The volume of the cylinder is: {volume:.2f}")
+print(f"The surface area of the cylinder is: {totalSurfaceArea:.2f} square meters")
+print(f"The volume of the cylinder is: {volume:.2f} cubic meters")
 
 time.sleep(10)
 os.system('cls' if os.name == 'nt' else 'clear')
 sys.exit()
-
-# Calculates volume and surface areas of cylinders.
-# Creative Commons Attribution-NonCommercial 4.0 International
-# Do not reupload without significant changes, do not use in ANY way for commercial ventures.
