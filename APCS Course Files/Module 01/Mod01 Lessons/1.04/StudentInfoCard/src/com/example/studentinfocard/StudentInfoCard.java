5cf90cdb745b8d94d7983eb7184dcf63be3f59bb
malikhass <136368985+malikhass@users.noreply.github.com>
2023-09-20 09:27:10 -0500

    1.04

---
+++ APCS Course Files/Module 01/Mod01 Lessons/1.04/StudentInfoCard/src/com/example/studentinfocard/StudentInfoCard.java
@@ -0,0 +1,37 @@
+// Coded by Malik Hassan on 09/19/2023
+// The purpose of this program is to act as a Student ID, but as a program.
+
+package com.example.studentinfocard;
+
+public class StudentInfoCard {
+	public static void main(String[] args)
+	{
+	   String banner = " .----..---. .-. .-..----. .----..-. .-. .---.    .-..----. " + System.lineSeparator() +
+			   		   "{ {__ {_   _}| { } || {}  \\| {_  |  `| |{_   _}   | || {}  \\" + System.lineSeparator() +
+			   		   ".-._} } | |  | {_} ||     /| {__ | |\\  |  | |     | ||     /" + System.lineSeparator() +
+			   		   "`----'  `-'  `-----'`----' `----'`-' `-'  `-'     `-'`----' " + System.lineSeparator() ;
+	   
+	   // Banner string
+	   
+	   System.out.println(banner);
+	   System.out.println();
+	   System.out.println("Name: Malik Hassan | " + "Birthday: May 13, 2009");
+	   System.out.println("School: John Dewey Academy of Learing | " + "Grade: 9 | " + "City: Green Bay, Wisconsin");
+	   System.out.println("Home Phone: N/A | " + "Cell Phone: 1-415-589-1635 | " + "Email: malihass@greenbaystudent.org");
+	   System.out.println("Available: 6:45 AM - 4:00 PM | Mon-Fri | " );
+	   System.out.println();
+	   System.out.println("Mother's Contact Information");
+	   System.out.println("Cell Phone: 1-510-230-9533 | " + "Email: kimberswen@gmail.com");
+	   System.out.println();
+	   System.out.println("Current Math Class: Algebra I");
+	   System.out.println();
+	   System.out.println("Programming Experience:");
+	   System.out.println("I am a Software Engineer that has been programming since late 2017, I have focused Python since late 2019, growing up in Berkely, CA I was constantly surrounded by the silicon valley environment, which led me to pick up a interest in programming.");
+	   System.out.println();
+	   System.out.println("Why I am taking this course:");
+	   System.out.print("I am taking this course because I would like to become a Software Engineer, and to be a well rounded Sotware Engineer I should learn Java. This course will prepare me for the college environment and I am happy that I have this resource.");
+	
+	   // Printing all of the information out.
+	   
+	} 
+}
