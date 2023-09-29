388d9cd8effb80bce33b58b501f2fef249ccf1ca
malikhass <136368985+malikhass@users.noreply.github.com>
2023-09-29 18:30:19 -0500

    1.11

---
+++ APCS Course Files/Module 01/Mod01 Lessons/1.11/src/com/example/grades/GradesV2.java
@@ -0,0 +1,25 @@
+package com.example.grades;
+
+public class GradesV2 {
+    public static void main(String[] args) {
+        int numTests = 0;      // counts number of tests
+        int testGrade = 0;     // individual test grade
+        int totalPoints = 0;   // total points for all tests
+        double average = 0.0;  // average grade
+
+        // Example test scores (you can replace these with your own)
+        int[] testScores = {95, 73, 91, 82};
+
+        for (int i = 0; i < testScores.length; i++) {
+            testGrade = testScores[i];
+            totalPoints += testGrade;
+            numTests++;
+            average = (double) totalPoints / numTests;
+
+            System.out.println("Test #" + numTests + " Score: " + testGrade);
+            System.out.println("Total Points: " + totalPoints);
+            System.out.println("Average Grade: " + average);
+            System.out.println();
+        }
+    }
+}
---
+++ APCS Course Files/Module 01/Mod01 Lessons/Testing/src/com/example/testing/Testing.java
@@ -0,0 +1,8 @@
+package com.example.testing;
+
+public class Testing {
+    public static void main(String[] args) {
+    	int iNum = 0;
+        System.out.println(iNum);
+    }
+}
