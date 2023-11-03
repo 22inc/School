3e025ea82603d4e283196588f7e70d7959644271
22inc <136368985+22inc@users.noreply.github.com>
2023-11-03 14:54:56 -0500

    h

--- APCS Course Files/Module 01/Mod01 Lessons/Testing/src/com/example/testing/Testing.java
+++ APCS Course Files/Module 01/Mod01 Lessons/Testing/src/com/example/testing/Testing.java
@@ -3,10 +3,11 @@ import java.util.Scanner;
 
 public class Testing {
     public static void main(String[] args) {
-    	Scanner in = new Scanner(System.in);
-    	// "Samira".equals("Sami")
-    	// "SAMI".compareTo("Samira") < 0
-    	// "Sami".compareTo("SAMI") < 0
-    	System.out.println("Sami".compareTo("SAMI") < 0);
+    	   int flipCount = 0;
+    	   while(flipCount <= 10)
+    	   {
+    	     System.out.println("test");
+    	     flipCount++;
+    	   } 
     }
 }
